a6119f885755ee7641dadffe19670a1e74b538fa
Byron Jones <byronjones77@gmail.com>
2023-09-13 01:35:13 -0400

    lab 1 - inital commit

---
+++ Lab_One/Lab1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> words = new List<string>();
+            Stopwatch stopwatch = new Stopwatch();
+            string choice;
+            do
+            {
+                Console.Write("Menu:\n" +
+                    "1 - Import words from File\n" +
+                    "2 - Bubble sort words\n" +
+                    "3 - LINQ/Lambda sort words\n" +
+                    "4 - Count the distinct words\n" +
+                    "5 - Take the last 10 words\n" +
+                    "6 - Reverse print the words\n" +
+                    "7 - Get and display words that end with 'a' and display the count\n" +
+                    "8 - Get and display words that start with 'm' and display the count\n" +
+                    "9 - Get and display words that are more than 3 characters long and start with the letter ‘a’\n" +
+                    "x - Exit\n" +
+                    "Enter your choice: ");
+
+                choice = Console.ReadLine();
+
+                if (choice != "1" && choice != "x" && !wordOperations.WordsLoaded((List<string>)words))
+                {
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case "1":
+                        words = wordOperations.ImportWordsFromFile();
+                        break;
+                    case "2":
+                        wordOperations.BubbleSort(words, stopwatch);
+                        break;
+                    case "3":
+                        wordOperations.LINQSort(words, stopwatch).ToList();
+                        break;
+                    case "4":
+                        wordOperations.CountDistinctWords(words);
+                        break;
+                    case "5":
+                        wordOperations.TakeLast10Words(words);
+                        break;
+                    case "6":
+                        wordOperations.ReversePrintWords(words);
+                        break;
+                    case "7":
+                        wordOperations.DisplayWordsEndingWithD(words);
+                        break;
+                    case "8":
+                        wordOperations.DisplayWordsContaingQ(words);
+                        break;
+                    case "9":
+                        wordOperations.DisplayWordsContainingA(words);
+                        break;
+                    case "x":
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            } while (choice != "x");
+        }
+    }
+}
---
+++ Lab_One/Lab1/WordOperations.cs
@@ -0,0 +1,131 @@
+﻿using System.Diagnostics;
+
+namespace Lab1{
+    static class wordOperations
+    {
+        public static bool WordsLoaded(List<string> words)
+        {
+            if (words.Count == 0)
+            {
+                Console.WriteLine("Please load words first!");
+                return false;
+            }
+            return true;
+        }
+
+        public static IList<string> ImportWordsFromFile()
+        {
+            IList<string> words = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader("Words.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        foreach (var word in line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries))
+                        {
+                            words.Add(word);
+                        }
+                    }
+                }
+                Console.WriteLine($"Imported {words.Count} words.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+            return words;
+        }
+
+        public static IList<string> BubbleSort(IList<string> words, Stopwatch timer)
+        {
+            var sortedWords = new List<string>(words);
+            bool swapped;
+
+            timer.Restart();
+
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < sortedWords.Count - 1; i++)
+                {
+                    if (string.Compare(sortedWords[i], sortedWords[i + 1]) > 0)
+                    {
+                        var temp = sortedWords[i];
+                        sortedWords[i] = sortedWords[i + 1];
+                        sortedWords[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+            timer.Stop();
+
+            Console.WriteLine($"Execution time: {timer.ElapsedMilliseconds} ms");
+            return sortedWords;
+        }
+
+        public static IList<string> LINQSort(IList<string> words, Stopwatch timer)
+        {
+            var sortedWords = new List<string>(words);
+
+            timer.Restart();
+
+            sortedWords = sortedWords.OrderBy(word => word).ToList();
+
+            timer.Stop();
+
+            Console.WriteLine($"Execution time: {timer.ElapsedMilliseconds} ms");
+            return sortedWords;
+        }
+
+        public static void CountDistinctWords(IList<string> words)
+        {
+            Console.WriteLine($"Number of distinct words: {words.Distinct().Count()}");
+        }
+
+        public static void TakeLast10Words(IList<string> words)
+        {
+            var last10Words = words.Reverse().Take(10).ToList();
+            last10Words.ForEach(word => Console.WriteLine(word));
+        }
+
+        public static IList<string>ReversePrintWords(IList<string> words)
+        {
+            var reversedWords = new List<string>(words);
+            reversedWords.Reverse();
+            reversedWords.ToList().ForEach(word => Console.WriteLine(word));
+            return reversedWords;
+        }
+
+        public static void DisplayWordsEndingWithD(IList<string> words)
+        {
+            var wordsEndingWithA = words.Where(w => w.EndsWith("d")).ToList();
+            Console.WriteLine($"The {wordsEndingWithA.Count()} words that end with 'a' are:");
+            foreach (var word in wordsEndingWithA)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        public static void DisplayWordsContaingQ(IList<string> words)
+        {
+            var wordsStartingWithM = words.Where(w => w.Contains("q")).ToList();
+            Console.WriteLine($"The {wordsStartingWithM.Count()} words that start with the letter 'm' are:");
+            foreach (var word in wordsStartingWithM)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        public static void DisplayWordsContainingA(IList<string> words)
+        {
+            var shortWordsContainingI = words.Where(w => w.Length > 3 && w.Contains("a")).ToList();
+            Console.WriteLine($"The {shortWordsContainingI.Count()} words that have more than 3 characters and include the letter 'a' are:");
+            foreach (var word in shortWordsContainingI)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
