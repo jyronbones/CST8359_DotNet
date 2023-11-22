ed56cde6d6eed3a4b2a4656d1cd031438c3b470f
Byron <80455013+jyronbones@users.noreply.github.com>
2023-11-22 18:13:24 -0500

    Lab 6 Submission

---
+++ Lab6/Lab6/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Lab6.Models
+{
+    public class Student
+    {
+        [Key]
+        [SwaggerSchema(ReadOnly = true)]
+        public Guid ID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Program { get; set; }
+    }
+}
