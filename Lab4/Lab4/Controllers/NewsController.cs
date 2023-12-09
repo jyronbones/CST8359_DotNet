a676496f48eb4bfec3133b3055f5ed16e424f024
Byron <80455013+jyronbones@users.noreply.github.com>
2023-12-09 18:11:33 -0500

    added delete implementation

--- Lab4/Lab4/Controllers/NewsController.cs
+++ Lab4/Lab4/Controllers/NewsController.cs
@@ -8,7 +8,6 @@ using System.IO;
 using System.Threading.Tasks;
 using Lab4.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Http.Extensions;
 
 public class NewsController : Controller
 {
@@ -20,13 +19,12 @@ public class NewsController : Controller
     {
         _context = context;
         _blobServiceClient = blobServiceClient;
-        //_containerName = configuration["AzureBlobStorageSettings:ContainerName"];
+        _containerName = configuration["AzureBlobStorageSettings:ContainerName"];
     }
 
     // GET: News/Create/{subscriptionId}
     public IActionResult Create(string subscriptionId)
     {
-        string _containerName = GetContainerNameFromSubscriptionId(subscriptionId);
         ViewData["SportClubId"] = new SelectList(_context.SportClubs, "Id", "Title");
         return View();
     }
@@ -36,7 +34,6 @@ public class NewsController : Controller
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Create(string subscriptionId, [Bind("NewsId,FileName,Url,SportClubId")] News news, IFormFile Photo)
     {
-        string _containerName = GetContainerNameFromSubscriptionId(subscriptionId);
         if (ModelState.IsValid)
         {
             try
@@ -56,13 +53,10 @@ public class NewsController : Controller
 
                     news.FileName = uniqueFileName;
                     news.Url = blobClient.Uri.ToString();
-                    var currentUrl = HttpContext.Request.GetDisplayUrl();
-                    news.SportClubId = currentUrl.Substring(Math.Max(0, currentUrl.Length - 2));
                 }
 
                 _context.News.Add(news);
                 await _context.SaveChangesAsync();
-                //return RedirectToAction(nameof(Index));
                 return Redirect($"/SportClubs/News/{news.SportClubId}");
             }
             catch (Exception ex)
@@ -75,8 +69,6 @@ public class NewsController : Controller
         return View(news);
     }
 
-
-
     // GET: News
     public async Task<IActionResult> Index()
     {
@@ -84,51 +76,52 @@ public class NewsController : Controller
         return View(newsList);
     }
 
-    // POST: News/Delete/5
-    [HttpPost, ActionName("Delete")]
+    // GET: News/Delete/5
+    // GET: News/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var news = await _context.News
+            .Include(n => n.SportClub)
+            .FirstOrDefaultAsync(m => m.NewsId == id);
+
+        if (news == null)
+        {
+            return NotFound();
+        }
+
+        ViewData["subscriptionId"] = news.SportClubId;
+        return View(news);
+    }
+
+    // POST: News/DeleteConfirmed/5
+    [HttpPost, ActionName("DeleteConfirmed")]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> DeleteConfirmed(int id)
     {
         var news = await _context.News.FindAsync(id);
-        if (news != null)
+        if (news == null)
         {
-            if (!string.IsNullOrEmpty(news.Url))
-            {
-                BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
-                BlobClient blobClient = containerClient.GetBlobClient(Path.GetFileName(news.Url));
-                await blobClient.DeleteIfExistsAsync();
-            }
-
-            _context.News.Remove(news);
-            await _context.SaveChangesAsync();
+            // Handle the case where the news item is not found
+            // This could redirect to an error page or back to the index with a message
+            return RedirectToAction(nameof(Index));
         }
 
-        return RedirectToAction(nameof(Index));
+        if (!string.IsNullOrEmpty(news.Url))
+        {
+            BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            BlobClient blobClient = containerClient.GetBlobClient(Path.GetFileName(news.Url));
+            await blobClient.DeleteIfExistsAsync();
+        }
+
+        _context.News.Remove(news);
+        await _context.SaveChangesAsync();
+
+        // Redirect back to the news list, possibly filtered by the sport club
+        return RedirectToAction(nameof(Index), new { subscriptionId = news.SportClubId });
     }
-
-    private string GetContainerNameFromSubscriptionId(string subscriptionId)
-    {
-        var currentUrl = HttpContext.Request.GetDisplayUrl();
-        var lastTwoChars = currentUrl.Substring(Math.Max(0, currentUrl.Length - 2));
-
-        string containerName = "";
-
-        
-            if (lastTwoChars.Equals("A1", StringComparison.OrdinalIgnoreCase))
-            {
-                containerName = "alpha";
-            }
-            else if (lastTwoChars.Equals("B1", StringComparison.OrdinalIgnoreCase))
-            {
-                containerName = "beta";
-            }
-            else if (lastTwoChars.Equals("O1", StringComparison.OrdinalIgnoreCase))
-            {
-                containerName = "omega";
-            }
-        
-
-        return containerName;
-    }
-
 }
